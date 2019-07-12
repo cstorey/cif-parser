12b2e0e6d8db23b0d776cae838082ca55fda5c29
Ceri Storey <cez@necrofish.org.uk>
2019-07-12 19:48:40 +0100

    Parse out CIF header.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,87 @@
-fn main() {
-    println!("Hello, world!");
+use std::borrow::Cow;
+use std::fs::File;
+use std::path::PathBuf;
+
+use failure::Fallible;
+use memmap::Mmap;
+use nom::{
+    bytes::streaming::*,
+    character::is_space,
+    character::streaming::*,
+    combinator::{cut, map, opt},
+    error::*,
+    Err, IResult,
+};
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "cif-parser", about = "CIF file parser")]
+struct Opts {
+    files: Vec<PathBuf>,
+}
+
+#[derive(Debug, Clone)]
+struct Header<'a> {
+    file_mainframe_identity: Cow<'a, str>,
+    extract_date: Cow<'a, str>,
+    extract_time: Cow<'a, str>,
+    current_file: Cow<'a, str>,
+    last_file: Cow<'a, str>,
+    update_indicator: char,
+    version: Cow<'a, str>,
+    user_start_date: Cow<'a, str>,
+    user_end_date: Cow<'a, str>,
+}
+
+fn parse<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], Header, E> {
+    let p = parse_header();
+    p(i)
+}
+
+fn parse_header<'a, E: ParseError<&'a [u8]>>() -> impl Fn(&'a [u8]) -> IResult<&'a [u8], Header, E>
+{
+    |i: &'a [u8]| -> IResult<&'a [u8], Header, E> {
+        let (i, _) = tag("HD")(i)?;
+        let (i, file_mainframe_identity) = take(20usize)(i)?;
+        let (i, extract_date) = take(6usize)(i)?;
+        let (i, extract_time) = take(4usize)(i)?;
+        let (i, current_file) = take(7usize)(i)?;
+        let (i, last_file) = take(7usize)(i)?;
+        let (i, update_indicator) = one_of(b"UF" as &[u8])(i)?;
+        let (i, version) = take(1usize)(i)?;
+        let (i, user_start_date) = take(6usize)(i)?;
+        let (i, user_end_date) = take(6usize)(i)?;
+        let (i, _spare) = take_while_m_n(20, 20, is_space)(i)?;
+
+        Ok((
+            i,
+            Header {
+                file_mainframe_identity: String::from_utf8_lossy(file_mainframe_identity),
+                extract_date: String::from_utf8_lossy(extract_date),
+                extract_time: String::from_utf8_lossy(extract_time),
+                current_file: String::from_utf8_lossy(current_file),
+                last_file: String::from_utf8_lossy(last_file),
+                update_indicator: update_indicator,
+                version: String::from_utf8_lossy(version),
+                user_start_date: String::from_utf8_lossy(user_start_date),
+                user_end_date: String::from_utf8_lossy(user_end_date),
+            },
+        ))
+    }
+}
+
+fn main() -> Fallible<()> {
+    let opts = Opts::from_args();
+
+    for f in opts.files {
+        let fp = File::open(f)?;
+        let mmap = unsafe { Mmap::map(&fp)? };
+        match parse::<VerboseError<_>>(&mmap) {
+            Ok((_rest, val)) => println!("Ok: {:#?}", val),
+
+            Err(err) => println!("Err: {:?}", err),
+        }
+    }
+
+    Ok(())
 }
