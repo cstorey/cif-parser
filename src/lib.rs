daf62609bd4f3f8716a77cbc95c7f32dddbc8342
Ceri Storey <cez@necrofish.org.uk>
2019-07-13 11:26:53 +0100

    Start on records sub-module.

--- src/lib.rs
+++ src/lib.rs
@@ -6,20 +6,9 @@ use nom::{
     combinator::map, error::*, sequence::terminated, IResult,
 };
 
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub enum Record<'a> {
-    Header(Header<'a>),
-    TiplocInsert(TiplocInsert<'a>),
-    TiplocAmend(TiplocAmend<'a>),
-    Association(Association<'a>),
-    BasicSchedule(BasicSchedule<'a>),
-    ScheduleExtra(ScheduleExtra<'a>),
-    LocationOrigin(LocationOrigin<'a>),
-    LocationIntermediate(LocationIntermediate<'a>),
-    LocationTerminating(LocationTerminating<'a>),
-    ChangeEnRoute(ChangeEnRoute<'a>),
-    Trailer(Trailer),
-}
+pub mod records;
+
+pub use records::parse;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum FullOrUpdate {
@@ -177,23 +166,6 @@ pub struct ChangeEnRoute<'a> {
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Trailer;
-
-pub fn parse<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], Record, E> {
-    let p = alt((
-        map(parse_header(), Record::Header),
-        map(parse_tiploc_insert(), Record::TiplocInsert),
-        map(parse_tiploc_amend(), Record::TiplocAmend),
-        map(parse_association(), Record::Association),
-        map(parse_basic_schedule(), Record::BasicSchedule),
-        map(parse_schedule_extra(), Record::ScheduleExtra),
-        map(parse_location_origin(), Record::LocationOrigin),
-        map(parse_location_intermediate(), Record::LocationIntermediate),
-        map(parse_location_terminating(), Record::LocationTerminating),
-        map(parse_change_en_route(), Record::ChangeEnRoute),
-        map(parse_trailer(), Record::Trailer),
-    ));
-    terminated(p, char('\n'))(i)
-}
 
 fn parse_header<'a, E: ParseError<&'a [u8]>>() -> impl Fn(&'a [u8]) -> IResult<&'a [u8], Header, E>
 {
---
+++ src/records/mod.rs
@@ -0,0 +1,34 @@
+use crate::*;
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub enum Record<'a> {
+    Header(Header<'a>),
+    TiplocInsert(TiplocInsert<'a>),
+    TiplocAmend(TiplocAmend<'a>),
+    Association(Association<'a>),
+    BasicSchedule(BasicSchedule<'a>),
+    ScheduleExtra(ScheduleExtra<'a>),
+    LocationOrigin(LocationOrigin<'a>),
+    LocationIntermediate(LocationIntermediate<'a>),
+    LocationTerminating(LocationTerminating<'a>),
+    ChangeEnRoute(ChangeEnRoute<'a>),
+    Trailer(Trailer),
+}
+
+pub fn parse<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], records::Record, E> {
+    use records::*;
+    let p = alt((
+        map(parse_header(), Record::Header),
+        map(parse_tiploc_insert(), Record::TiplocInsert),
+        map(parse_tiploc_amend(), Record::TiplocAmend),
+        map(parse_association(), Record::Association),
+        map(parse_basic_schedule(), Record::BasicSchedule),
+        map(parse_schedule_extra(), Record::ScheduleExtra),
+        map(parse_location_origin(), Record::LocationOrigin),
+        map(parse_location_intermediate(), Record::LocationIntermediate),
+        map(parse_location_terminating(), Record::LocationTerminating),
+        map(parse_change_en_route(), Record::ChangeEnRoute),
+        map(parse_trailer(), Record::Trailer),
+    ));
+    terminated(p, char('\n'))(i)
+}
