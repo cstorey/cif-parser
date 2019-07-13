98074e10ed268f0537481fffd426e92dfe7abafa
Ceri Storey <cez@necrofish.org.uk>
2019-07-13 10:54:08 +0100

    Parse location terminating.

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,7 @@ enum Record<'a> {
     ScheduleExtra(ScheduleExtra<'a>),
     LocationOrigin(LocationOrigin<'a>),
     LocationIntermediate(LocationIntermediate<'a>),
+    LocationTerminating(LocationTerminating<'a>),
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -157,6 +158,16 @@ struct LocationIntermediate<'a> {
     perf_allowance: Cow<'a, str>,
 }
 
+#[derive(Debug, Clone, Eq, PartialEq)]
+struct LocationTerminating<'a> {
+    tiploc: Cow<'a, str>,
+    scheduled_arrival_time: Cow<'a, str>,
+    public_arrival: Cow<'a, str>,
+    platform: Cow<'a, str>,
+    path: Cow<'a, str>,
+    activity: Cow<'a, str>,
+}
+
 fn parse<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], Record, E> {
     let p = alt((
         map(parse_header(), Record::Header),
@@ -167,6 +178,7 @@ fn parse<'a, E: ParseError<&'a [u8]>>(i: &'a [u8]) -> IResult<&'a [u8], Record, 
         map(parse_schedule_extra(), Record::ScheduleExtra),
         map(parse_location_origin(), Record::LocationOrigin),
         map(parse_location_intermediate(), Record::LocationIntermediate),
+        map(parse_location_terminating(), Record::LocationTerminating),
     ));
     terminated(p, char('\n'))(i)
 }
@@ -401,7 +413,6 @@ fn parse_location_origin<'a, E: ParseError<&'a [u8]>>(
         ))
     }
 }
-
 fn parse_location_intermediate<'a, E: ParseError<&'a [u8]>>(
 ) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], LocationIntermediate, E> {
     |i: &'a [u8]| -> IResult<&'a [u8], LocationIntermediate, E> {
@@ -437,6 +448,32 @@ fn parse_location_intermediate<'a, E: ParseError<&'a [u8]>>(
                 eng_allowance: String::from_utf8_lossy(eng_allowance),
                 path_allowance: String::from_utf8_lossy(path_allowance),
                 perf_allowance: String::from_utf8_lossy(perf_allowance),
+            },
+        ))
+    }
+}
+
+fn parse_location_terminating<'a, E: ParseError<&'a [u8]>>(
+) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], LocationTerminating, E> {
+    |i: &'a [u8]| -> IResult<&'a [u8], LocationTerminating, E> {
+        let (i, _) = tag("LT")(i)?;
+        let (i, tiploc) = take(8usize)(i)?;
+        let (i, scheduled_arrival_time) = take(5usize)(i)?;
+        let (i, public_arrival) = take(4usize)(i)?;
+        let (i, platform) = take(3usize)(i)?;
+        let (i, path) = take(3usize)(i)?;
+        let (i, activity) = take(12usize)(i)?;
+        let (i, _spare) = take_while_m_n(43, 43, is_space)(i)?;
+
+        Ok((
+            i,
+            LocationTerminating {
+                tiploc: String::from_utf8_lossy(tiploc),
+                scheduled_arrival_time: String::from_utf8_lossy(scheduled_arrival_time),
+                public_arrival: String::from_utf8_lossy(public_arrival),
+                platform: String::from_utf8_lossy(platform),
+                path: String::from_utf8_lossy(path),
+                activity: String::from_utf8_lossy(activity),
             },
         ))
     }
@@ -659,6 +696,29 @@ mod test {
                     path_allowance: "  ".into(),
                     activity: "T           ".into(),
                     perf_allowance: "  ".into(),
+                },
+                "",
+            )
+        )
+    }
+
+    #[test]
+    fn should_parse_location_terminating() {
+        let p = parse_location_terminating::<VerboseError<_>>();
+        let i = b"LTTUNWELL 0125 01271     TF                                                     ";
+        assert_eq!(80, i.len());
+        let (rest, val) = p(i).expect("parse");
+        let rest = String::from_utf8_lossy(rest);
+        assert_eq!(
+            (val, &*rest),
+            (
+                LocationTerminating {
+                    tiploc: "TUNWELL ".into(),
+                    scheduled_arrival_time: "0125 ".into(),
+                    public_arrival: "0127".into(),
+                    platform: "1  ".into(),
+                    path: "   ".into(),
+                    activity: "TF          ".into(),
                 },
                 "",
             )
