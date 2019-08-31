5fd8a53504ed8c2d65cfca5c47e76ecd6b7757a7
Ceri Storey <cez@necrofish.org.uk>
2019-08-31 14:53:19 +0100

    Convert the rest of header to use helpers.

--- src/header.rs
+++ src/header.rs
@@ -20,7 +20,7 @@ pub struct Header<'a> {
     pub extract_date: Cow<'a, str>,
     pub extract_time: Cow<'a, str>,
     pub current_file: Cow<'a, str>,
-    pub last_file: Cow<'a, str>,
+    pub last_file: Option<Cow<'a, str>>,
     pub update_indicator: FullOrUpdate,
     pub version: Cow<'a, str>,
     pub user_start_date: Cow<'a, str>,
@@ -34,14 +34,14 @@ pub(super) fn parse_header<'a>() -> impl Fn(&'a [u8]) -> IResult<&'a [u8], Heade
         let (i, extract_date) = mandatory(string(6usize))(i)?;
         let (i, extract_time) = mandatory(string(4usize))(i)?;
         let (i, current_file) = mandatory(string(7usize))(i)?;
-        let (i, last_file) = take(7usize)(i)?;
+        let (i, last_file) = string(7usize)(i)?;
         let (i, update_indicator) = alt((
             map(char('U'), |_| FullOrUpdate::Update),
             map(char('F'), |_| FullOrUpdate::Full),
         ))(i)?;
-        let (i, version) = take(1usize)(i)?;
-        let (i, user_start_date) = take(6usize)(i)?;
-        let (i, user_end_date) = take(6usize)(i)?;
+        let (i, version) = mandatory(string(1usize))(i)?;
+        let (i, user_start_date) = mandatory(string(6usize))(i)?;
+        let (i, user_end_date) = mandatory(string(6usize))(i)?;
         let (i, _spare) = take_while_m_n(20, 20, is_space)(i)?;
 
         Ok((
@@ -51,11 +51,11 @@ pub(super) fn parse_header<'a>() -> impl Fn(&'a [u8]) -> IResult<&'a [u8], Heade
                 extract_date: extract_date.into(),
                 extract_time: extract_time.into(),
                 current_file: current_file.into(),
-                last_file: String::from_utf8_lossy(last_file),
+                last_file: last_file.map(Into::into),
                 update_indicator: update_indicator,
-                version: String::from_utf8_lossy(version),
-                user_start_date: String::from_utf8_lossy(user_start_date),
-                user_end_date: String::from_utf8_lossy(user_end_date),
+                version: version.into(),
+                user_start_date: user_start_date.into(),
+                user_end_date: user_end_date.into(),
             },
         ))
     }
