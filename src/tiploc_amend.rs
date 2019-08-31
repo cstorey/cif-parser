15480d75e7c086fd1f8e1ce0ce8704e55c7f2191
Ceri Storey <cez@necrofish.org.uk>
2019-08-31 18:09:53 +0100

    Use helpers in tiploc amend.

--- src/tiploc_amend.rs
+++ src/tiploc_amend.rs
@@ -1,46 +1,47 @@
-use std::borrow::Cow;
+use nom::{bytes::streaming::*, character::is_space, IResult};
 
-use nom::{bytes::streaming::*, character::is_space, error::*, IResult};
+use crate::errors::CIFParseError;
+use crate::helpers::{mandatory_str, string};
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct TiplocAmend<'a> {
-    pub tiploc: Cow<'a, str>,
-    pub nlc: Cow<'a, str>,
-    pub nlc_check: Cow<'a, str>,
-    pub tps_description: Cow<'a, str>,
-    pub stanox: Cow<'a, str>,
-    pub crs: Cow<'a, str>,
-    pub nlc_desc: Cow<'a, str>,
-    pub new_tiploc: Cow<'a, str>,
+    pub tiploc: &'a str,
+    pub nlc: &'a str,
+    pub nlc_check: &'a str,
+    pub tps_description: &'a str,
+    pub stanox: &'a str,
+    pub crs: Option<&'a str>,
+    pub nlc_desc: Option<&'a str>,
+    pub new_tiploc: Option<&'a str>,
 }
 
-pub(super) fn parse_tiploc_amend<'a, E: ParseError<&'a [u8]>>(
-) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], TiplocAmend, E> {
-    |i: &'a [u8]| -> IResult<&'a [u8], TiplocAmend, E> {
+pub(super) fn parse_tiploc_amend<'a>(
+) -> impl Fn(&'a [u8]) -> IResult<&'a [u8], TiplocAmend, CIFParseError> {
+    |i: &'a [u8]| -> IResult<&'a [u8], TiplocAmend, CIFParseError> {
         let (i, _) = tag("TA")(i)?;
-        let (i, tiploc) = take(7usize)(i)?;
-        let (i, _) = take(2usize)(i)?; // `capitals`
-        let (i, nlc) = take(6usize)(i)?;
-        let (i, nlc_check) = take(1usize)(i)?;
-        let (i, tps_description) = take(26usize)(i)?;
-        let (i, stanox) = take(5usize)(i)?;
-        let (i, _po_code) = take(4usize)(i)?;
-        let (i, crs) = take(3usize)(i)?;
-        let (i, nlc_desc) = take(16usize)(i)?;
-        let (i, new_tiploc) = take(7usize)(i)?;
+        let (i, tiploc) = mandatory_str(7usize)(i)?;
+        let (i, _) = mandatory_str(2usize)(i)?; // `capitals`
+        let (i, nlc) = mandatory_str(6usize)(i)?;
+        let (i, nlc_check) = mandatory_str(1usize)(i)?;
+        let (i, tps_description) = mandatory_str(26usize)(i)?;
+        let (i, stanox) = mandatory_str(5usize)(i)?;
+        let (i, _po_code) = mandatory_str(4usize)(i)?;
+        let (i, crs) = string(3usize)(i)?;
+        let (i, nlc_desc) = string(16usize)(i)?;
+        let (i, new_tiploc) = string(7usize)(i)?;
         let (i, _spare) = take_while_m_n(1, 1, is_space)(i)?;
 
         Ok((
             i,
             TiplocAmend {
-                tiploc: String::from_utf8_lossy(tiploc),
-                nlc: String::from_utf8_lossy(nlc),
-                nlc_check: String::from_utf8_lossy(nlc_check),
-                tps_description: String::from_utf8_lossy(tps_description),
-                stanox: String::from_utf8_lossy(stanox),
-                crs: String::from_utf8_lossy(crs),
-                nlc_desc: String::from_utf8_lossy(nlc_desc),
-                new_tiploc: String::from_utf8_lossy(new_tiploc),
+                tiploc: tiploc,
+                nlc: nlc,
+                nlc_check: nlc_check,
+                tps_description: tps_description,
+                stanox: stanox,
+                crs: crs,
+                nlc_desc: nlc_desc,
+                new_tiploc: new_tiploc,
             },
         ))
     }
@@ -52,8 +53,8 @@ mod test {
     use nom::combinator::complete;
 
     #[test]
-    fn should_parse_tiploc_amend() {
-        let p = complete(parse_tiploc_amend::<VerboseError<_>>());
+    fn should_parse_tiploc_amend_a() {
+        let p = complete(parse_tiploc_amend());
         let hdr =
             b"TAMBRK94200590970AMILLBROOK SIG E942        86536   0                           ";
         assert_eq!(80, hdr.len());
@@ -65,11 +66,11 @@ mod test {
                 tiploc: "MBRK942".into(),
                 nlc: "590970".into(),
                 nlc_check: "A".into(),
-                tps_description: "MILLBROOK SIG E942        ".into(),
+                tps_description: "MILLBROOK SIG E942".into(),
                 stanox: "86536".into(),
-                crs: "   ".into(),
-                nlc_desc: "                ".into(),
-                new_tiploc: "       ".into(),
+                crs: None,
+                nlc_desc: None,
+                new_tiploc: None,
             }
         )
     }
