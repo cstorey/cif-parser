52e0fb24434dc15fabc92a4b625dec26a80e2240
Ceri Storey <cez@necrofish.org.uk>
2019-08-31 15:44:26 +0100

    Use helpers for tiploc inserts.

--- src/tiploc_insert.rs
+++ src/tiploc_insert.rs
@@ -3,6 +3,7 @@ use std::borrow::Cow;
 use nom::{bytes::streaming::*, character::is_space, IResult};
 
 use crate::errors::CIFParseError;
+use crate::helpers::*;
 use crate::tiploc::Tiploc;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -21,26 +22,26 @@ pub(super) fn parse_tiploc_insert<'a>(
     |i: &'a [u8]| -> IResult<&'a [u8], TiplocInsert, CIFParseError> {
         let (i, _) = tag("TI")(i)?;
         let (i, tiploc) = Tiploc::parse(i)?;
-        let (i, _) = take(2usize)(i)?; // `capitals`
-        let (i, nlc) = take(6usize)(i)?;
-        let (i, nlc_check) = take(1usize)(i)?;
-        let (i, tps_description) = take(26usize)(i)?;
-        let (i, stanox) = take(5usize)(i)?;
-        let (i, _po_code) = take(4usize)(i)?;
-        let (i, crs) = take(3usize)(i)?;
-        let (i, nlc_desc) = take(16usize)(i)?;
+        let (i, _) = string(2usize)(i)?; // `capitals`
+        let (i, nlc) = mandatory(string(6usize))(i)?;
+        let (i, nlc_check) = mandatory(string(1usize))(i)?;
+        let (i, tps_description) = mandatory(string(26usize))(i)?;
+        let (i, stanox) = mandatory(string(5usize))(i)?;
+        let (i, _po_code) = mandatory(string(4usize))(i)?;
+        let (i, crs) = mandatory(string(3usize))(i)?;
+        let (i, nlc_desc) = mandatory(string(16usize))(i)?;
         let (i, _spare) = take_while_m_n(8, 8, is_space)(i)?;
 
         Ok((
             i,
             TiplocInsert {
                 tiploc: tiploc,
-                nlc: String::from_utf8_lossy(nlc),
-                nlc_check: String::from_utf8_lossy(nlc_check),
-                tps_description: String::from_utf8_lossy(tps_description),
-                stanox: String::from_utf8_lossy(stanox),
-                crs: String::from_utf8_lossy(crs),
-                nlc_desc: String::from_utf8_lossy(nlc_desc),
+                nlc: nlc.into(),
+                nlc_check: nlc_check.into(),
+                tps_description: tps_description.into(),
+                stanox: stanox.into(),
+                crs: crs.into(),
+                nlc_desc: nlc_desc.into(),
             },
         ))
     }
@@ -64,7 +65,7 @@ mod test {
                 tiploc: Tiploc::from_str("BLTNODR"),
                 nlc: "853600".into(),
                 nlc_check: "D".into(),
-                tps_description: "BOLTON-UPON-DEARNE        ".into(),
+                tps_description: "BOLTON-UPON-DEARNE".into(),
                 stanox: "24011".into(),
                 crs: "BTD".into(),
                 nlc_desc: "BOLTON ON DEARNE".into(),
