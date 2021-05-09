ac0a28ea997a83565171168492fbd9480cc37814
Ceri Storey <cez@necrofish.org.uk>
2021-05-09 11:17:50 +0100

    Use thiserror for CIFParseError.

--- src/errors.rs
+++ src/errors.rs
@@ -1,26 +1,16 @@
-use std::fmt;
-
 use bytes::Bytes;
+use thiserror::Error;
 
-const SNIPPET_LEN: usize = 240;
-
-#[derive(Debug)]
+#[derive(Debug, Error)]
 pub enum CIFParseError {
+    #[error("UTF conversion: {}", 0)]
     Utf8(std::str::Utf8Error),
+    #[error("Invalid number: {}", 0)]
     InvalidNumber(lexical_core::Error),
+    #[error("Invalid time: {:?}", 0)]
     InvalidTime(Bytes),
+    #[error("Invalid item")]
     InvalidItem,
-}
-
-impl fmt::Display for CIFParseError {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            CIFParseError::Utf8(ref err) => writeln!(fmt, "UTF conversion: {}", err),
-            CIFParseError::InvalidNumber(e) => writeln!(fmt, "Invalid number: {:?}", e),
-            CIFParseError::InvalidTime(s) => writeln!(fmt, "Invalid time: {}", as_snippet(s)),
-            CIFParseError::InvalidItem => writeln!(fmt, "Invalid item"),
-        }
-    }
 }
 
 impl std::convert::From<std::str::Utf8Error> for CIFParseError {
@@ -32,13 +22,4 @@ impl std::convert::From<lexical_core::Error> for CIFParseError {
     fn from(e: lexical_core::Error) -> Self {
         CIFParseError::InvalidNumber(e)
     }
-}
-
-fn as_snippet(s: &[u8]) -> String {
-    let len = std::cmp::min(s.len(), SNIPPET_LEN);
-    format!(
-        "{}{}",
-        String::from_utf8_lossy(&s[..len]),
-        if s.len() < SNIPPET_LEN { "" } else { "…" }
-    )
 }
